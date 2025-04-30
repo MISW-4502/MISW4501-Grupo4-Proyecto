7c96b204c1d802aa9086ba8097fd29dfbc484645
Javier andres acevedo buitrago <ja.acevedob1@uniandes.edu.co>
2025-04-30 09:53:51 -0500

    Config CORS

--- server_backend/manufacturer/main.py
+++ server_backend/manufacturer/main.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from src.routes.manufacturer_routes import manufacturer_bp
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:4200", "https://proyecto-grupo-4-73827233792.us-central1.run.app"])
 app.register_blueprint(manufacturer_bp, url_prefix="/manufacturer")
 
 if __name__ == '__main__':
--- server_backend/sales/main.py
+++ server_backend/sales/main.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from src.routes.sales_routes import sales_bp
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:4200", "https://proyecto-grupo-4-73827233792.us-central1.run.app"])
 app.register_blueprint(sales_bp, url_prefix="/sales")
 
 if __name__ == '__main__':
